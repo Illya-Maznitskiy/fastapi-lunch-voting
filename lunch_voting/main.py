adc2521197bbb2116fdc55c10f8865d300ceda7c
Illya Maznitskiy <comercaleuros@gmail.com>
2025-03-09 16:33:41 +0200

    Add all routers to main.py

--- lunch_voting/main.py
+++ lunch_voting/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 
+from routers import menu, restaurant, user, vote
+
+# Initialize the FastAPI application
 app = FastAPI()
 
 
+app.include_router(menu.router)
+app.include_router(restaurant.router)
+app.include_router(user.router)
+app.include_router(vote.router)
+
+
+# A simple root endpoint to verify the app is running
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
